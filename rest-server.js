aeb2367a3ebd186a60a40e7873db2c206a972809
Sumit Bisht <sumitbisht1987@gmail.com>
2016-11-11 03:26:02 +0530

    added REST service

---
+++ rest-server.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+	restful = require('node-restful'),
+	mongoose = restful.mongoose;
+
+var app = express();
+
+//app.configure(function(){
+//	app.use(express.bodyParser());
+//	app.use(express.methodOverride());
+//});
+
+mongoose.connect('mongodb://localhost/restful');
+
+var ProductSchema = mongoose.Schema({
+	name: String,
+	sku: String,
+	price: Number
+});
+
+var Product = restful.model('product', ProductSchema);
+Product.methods(['get', 'put', 'post', 'delete']);
+Product.register(app, '/api/v1/products');
+
+app.listen(3000);
+console.log('Server is up at port 3000');
